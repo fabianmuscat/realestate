88f5e9f80f449561b5e42642fded9c71046614b1
Fabian Muscat <fabian.muscat@icloud.com>
2024-02-18 17:02:11 +0100

    Set-up laravel breeze

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->string('role',  20)->default('user'); // admin, user
+            $table->string('status',  15)->default('active'); // active, blocked, deleted
             $table->rememberToken();
             $table->timestamps();
         });
